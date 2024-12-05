84ed78fd9dae6d3ccaf7baac0f7c8124ef425529
Ibrahim Khalilov <ibrahim.khalilov@yahoo.com>
2024-12-05 16:06:42 -0500

    add implementation

--- server/app/src/main/java/business/order/DefaultOrderService.java
+++ server/app/src/main/java/business/order/DefaultOrderService.java
@@ -1,17 +1,30 @@
 package business.order;
 
 import api.ApiException;
+import business.BookstoreDbException;
+import business.JdbcUtils;
 import business.book.Book;
 import business.book.BookDao;
 import business.cart.ShoppingCart;
+import business.cart.ShoppingCartItem;
+import business.customer.Customer;
 import business.customer.CustomerForm;
+import business.customer.CustomerDao;
 
+import java.sql.Connection;
+import java.util.Date;
+import java.sql.SQLException;
 import java.time.DateTimeException;
 import java.time.YearMonth;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class DefaultOrderService implements OrderService {
 
 	private BookDao bookDao;
+	private OrderDao orderDao;
+	private CustomerDao customerDao;
+	private LineItemDao lineItemDao;
 
 	public void setBookDao(BookDao bookDao) {
 		this.bookDao = bookDao;
@@ -19,17 +32,74 @@ public class DefaultOrderService implements OrderService {
 
 	@Override
 	public OrderDetails getOrderDetails(long orderId) {
-		// NOTE: THIS METHOD PROVIDED NEXT PROJECT
-		return null;
+		Order order = orderDao.findByOrderId(orderId);
+		Customer customer = customerDao.findByCustomerId(order.customerId());
+		List<LineItem> lineItems = lineItemDao.findByOrderId(orderId);
+		List<Book> books = lineItems
+				.stream()
+				.map(lineItem -> bookDao.findByBookId(lineItem.bookId()))
+				.toList();
+		return new OrderDetails(order, customer, lineItems, books);
 	}
 
 	@Override
 	public long placeOrder(CustomerForm customerForm, ShoppingCart cart) {
 		validateCustomer(customerForm);
 		validateCart(cart);
-		// NOTE: MORE CODE PROVIDED NEXT PROJECT
+		try (Connection connection = JdbcUtils.getConnection()) {
+			Date ccExpDate = getCardExpirationDate(
+					customerForm.getCcExpiryMonth(),
+					customerForm.getCcExpiryYear());
+			return performPlaceOrderTransaction(
+					customerForm.getName(),
+					customerForm.getAddress(),
+					customerForm.getPhone(),
+					customerForm.getEmail(),
+					customerForm.getCcNumber(),
+					ccExpDate, cart, connection);
+		} catch (SQLException e) {
+			throw new BookstoreDbException("Error during close connection for customer order", e);
+		}
+	}
 
-		return -1;
+	private Date getCardExpirationDate(String monthString, String yearString) {
+		int month = Integer.parseInt(monthString);
+		int year = Integer.parseInt(yearString);
+		YearMonth yearMonth = YearMonth.of(year, month);
+		return java.sql.Date.valueOf(yearMonth.atEndOfMonth());
+	}
+
+	private long performPlaceOrderTransaction(
+			String name, String address, String phone,
+			String email, String ccNumber, Date date,
+			ShoppingCart cart, Connection connection) {
+		try {
+			connection.setAutoCommit(false);
+			long customerId = customerDao.create(
+					connection, name, address, phone, email,
+					ccNumber, date);
+			long customerOrderId = orderDao.create(
+					connection,
+					cart.getComputedSubtotal() + cart.getSurcharge(),
+					generateConfirmationNumber(), customerId);
+			for (ShoppingCartItem item : cart.getItems()) {
+				lineItemDao.create(connection, customerOrderId,
+						item.getBookId(), item.getQuantity());
+			}
+			connection.commit();
+			return customerOrderId;
+		} catch (Exception e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				throw new BookstoreDbException("Failed to roll back transaction", e1);
+			}
+			return 0;
+		}
+	}
+
+	private int generateConfirmationNumber() {
+		return ThreadLocalRandom.current().nextInt(999999999);
 	}
 
 	private void validateCustomer(CustomerForm customerForm) {
